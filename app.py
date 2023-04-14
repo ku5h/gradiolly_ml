0254795b63300ac66267b280bbb7095a74e67493
kush <kush@hike.in>
2023-04-14 23:49:52 +0530

    tabbed interface

--- app.py
+++ app.py
@@ -2,10 +2,22 @@ import gradio as gr
 
 gr.close_all() # close all running gradio processes (else you might get 'Port In Use' error)
 
-def user_greeting(name):
-    return "Hi! " + name + " Welcome to ECO6800, yet again!"
-    
-#define gradio interface and other parameters
-app =  gr.Interface(fn = user_greeting, inputs="text", outputs="text")
+title = "Tabbed Interface & Labelled Boxes"
+
+#app 1
+def greet(name):
+    return "Hi! " + name + "! This seems to easier than flask, ain't it?"
+
+#app 2
+def help(do):
+    return "Please focus on the class, and you won't need this help section! 😎"
+
+#interface 1
+app1 =  gr.Interface(fn = greet, inputs="text", outputs="text")
+#interface 2
+
+app2 =  gr.Interface(fn = help, inputs=gr.Textbox("Question"), outputs=gr.Textbox(label='Answer'))
+
+app = gr.TabbedInterface([app1, app2], ["Welcome", "Help"])
 
 app.launch(server_port=8080)
