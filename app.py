6f855b1a04d22cfae54299a1abb952e18de5faf0
kush <kush@hike.in>
2023-04-15 00:22:25 +0530

    dataframes

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import gradio as gr
+import pandas as pd
+import numpy as np
 
 gr.close_all() # close all running gradio processes (else you might get 'Port In Use' error)
+PORT = 8080
 
-title = "Tabbed Interface & Labelled Boxes"
+title = "Dataframes"
 
 #app 1
 def greet(name):
@@ -12,12 +15,17 @@ def greet(name):
 def help(do):
     return "Please focus on the class, and you won't need this help section! 😎"
 
-#interface 1
+def display_randn_matrix(rows, columns):
+    #create a random matrix of size m x n
+    matrix = np.random.rand(int(rows), int(columns))
+    # round a matrix to 2 decimal places
+    matrix = np.round(matrix, 2)
+    return pd.DataFrame(matrix)
+
 app1 =  gr.Interface(fn = greet, inputs="text", outputs="text")
-#interface 2
+app2 = gr.Interface(fn = display_randn_matrix, inputs=["number", "number"], outputs=gr.DataFrame(label="Matrix"))
+app3 =  gr.Interface(fn = help, inputs=gr.Textbox("Question"), outputs=gr.Textbox(label='Answer'))
 
-app2 =  gr.Interface(fn = help, inputs=gr.Textbox("Question"), outputs=gr.Textbox(label='Answer'))
+app = gr.TabbedInterface([app1, app2, app3], ["Welcome", "Matrix", "Help"])
 
-app = gr.TabbedInterface([app1, app2], ["Welcome", "Help"])
-
-app.launch(server_port=8080)
+app.launch(server_port=PORT)
