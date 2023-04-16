1072a86e28d1b5f63a18da8059a4165ae3274408
kush <kush@hike.in>
2023-04-16 14:07:57 +0530

    plotting

--- app.py
+++ app.py
@@ -1,31 +1,45 @@
 import gradio as gr
 import pandas as pd
 import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 
 gr.close_all() # close all running gradio processes (else you might get 'Port In Use' error)
 PORT = 8080
 
-title = "Dataframes"
+title = "Plotting"
 
-#app 1
-def greet(name):
-    return "Hi! " + name + "! This seems to easier than flask, ain't it?"
-
-#app 2
-def help(do):
-    return "Please focus on the class, and you won't need this help section! 😎"
-
-def display_randn_matrix(rows, columns):
+def plot_bivariate_normal(num_points):
     #create a random matrix of size m x n
-    matrix = np.random.rand(int(rows), int(columns))
+    matrix = np.random.randn(int(num_points), 2)
     # round a matrix to 2 decimal places
     matrix = np.round(matrix, 2)
-    return pd.DataFrame(matrix)
+    df = pd.DataFrame(matrix)
+    # create plotly scatter and heatmap plots from the first two columns of df
+    fig = px.scatter(df, x=df.columns[0], y=df.columns[1])
+    fig2 = px.density_heatmap(df, x=df.columns[0], y=df.columns[1])
 
-app1 =  gr.Interface(fn = greet, inputs="text", outputs="text")
-app2 = gr.Interface(fn = display_randn_matrix, inputs=["number", "number"], outputs=gr.DataFrame(label="Matrix"))
-app3 =  gr.Interface(fn = help, inputs=gr.Textbox("Question"), outputs=gr.Textbox(label='Answer'))
+    return fig, fig2
 
-app = gr.TabbedInterface([app1, app2, app3], ["Welcome", "Matrix", "Help"])
+def plot_mount_bruno():
+    # Read data from a csv
+    z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
+
+    fig = go.Figure(data=[go.Surface(z=z_data.values)])
+    fig.update_traces(contours_z=dict(show=True, usecolormap=True,
+                                    highlightcolor="limegreen", project_z=True))
+    fig.update_layout(title='Mt Bruno Elevation', autosize=False,
+                    scene_camera_eye=dict(x=1.87, y=0.88, z=-0.64),
+                    width=500, height=500,
+                    margin=dict(l=65, r=50, b=65, t=90)
+    )
+
+    return fig
+    
+bi_normal = gr.Interface(fn = plot_bivariate_normal, inputs="number", 
+                   outputs=[gr.Plot(label='Scatter'), gr.Plot(label='Density Heatmap')])
+mount_bruno = gr.Interface(fn = plot_mount_bruno, inputs=None, outputs=[gr.Plot(label='Mount Bruno Elevation')])
+
+app = gr.TabbedInterface([bi_normal, mount_bruno], tab_names=['Bivariate Normal', 'Mount Bruno'])
 
 app.launch(server_port=PORT)
